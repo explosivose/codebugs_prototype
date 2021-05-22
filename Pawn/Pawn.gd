23e9e408faaded80fb595090955e49492bfe9513
Matt Blickem <explosivose@gmail.com>
2021-05-22 16:54:01 +0200

    add pathfinding lines to pawn

--- Pawn/Pawn.gd
+++ Pawn/Pawn.gd
@@ -4,24 +4,43 @@ class_name Pawn
 
 signal destination_reached
 
+const LINE_SCENE_PATH = "res://Line/Line.tscn"
+
 onready var terrain: Terrain = get_tree().current_scene.get_node("terrain")
 var moving = false
 var at_destination = false
 var _destination: Vector2 = position
 var _path: PoolVector2Array
-
+onready var _line = {
+	intent = load(LINE_SCENE_PATH).instance() as Line2D,
+	path = load(LINE_SCENE_PATH).instance() as Line2D,
+}
 
 func _ready():
-	pass # Replace with function body.
+	_line['intent'].width = 2
+	_line['intent'].default_color= '#ff6678'
+	get_parent().call_deferred('add_child', _line['intent'])
+	get_parent().call_deferred('add_child', _line['path'])
+
+func _hide_line(name: String):
+	_line[name].hide()
+	
+func _show_line(name: String, points: PoolVector2Array):
+	if points:
+		_line[name].points = points
+	_line[name].show()
 
 func move_to(destination: Vector2):
 	_destination = destination
-	_path = terrain.get_terrain_path_world(position, destination)
+	_show_line('intent', [position, _destination])
+	_path = terrain.get_terrain_path(position, destination)
+	_show_line('path', _path)
 	at_destination = false
 	Logger.fine('Pawn move to ' + str(_path))
 
 func _process(delta: float):
 	if _path.size() > 1:
+		_show_line('intent', [position, _path[1]])
 		var d: float = position.distance_to(_path[1])
 		if d > 5:
 			moving = true
@@ -30,8 +49,10 @@ func _process(delta: float):
 		else:
 			_path.remove(1)
 		_path[0] = position
+		_show_line('path', _path)
 	else:
 		if not at_destination:
 			at_destination = true
-			emit_signal("destination_reached")
+			_hide_line('path')
+			emit_signal('destination_reached')
 		moving = false
